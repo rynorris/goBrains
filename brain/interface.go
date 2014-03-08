a30371e259498cf43b210f7df39a85db7e81992b
= <=>
2014-03-08 12:27:51 +0000

    Moved to using new interfaces. Added Charge as an abstract object to be embedded into brain elements.

--- brain/interface.go
+++ brain/interface.go
@@ -10,11 +10,11 @@ package brain
 // Chargeable defines the family of objects that participate in a neural network.
 // Objects that implement the chargable interface accept charge from other objects in the neural net.
 type Chargeable interface {
-	Charge(strength Charge)
+    Charge(strength ChargeUnit)
 }
 
 // Worker defines the family of objects which can be added to the neural worker queue and are scheduled for later work.
 // Objects that implement the worker interface perform actions only during a work process.
 type Worker interface {
-	Work()
+    Work()
 }
