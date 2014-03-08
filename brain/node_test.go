f3d9d590a6398381f30bf8e6fb6a2fd70d6b2a1b
= <=>
2014-03-08 12:27:51 +0000

    Moved to using new interfaces. Added Charge as an abstract object to be embedded into brain elements.

--- brain/node_test.go
+++ brain/node_test.go
@@ -5,71 +5,68 @@ import "testing"
 const delta = 0.0001
 
 func TestNew(t *testing.T) {
-	n := NewNode()
+    n := NewNode()
 
-	if n.firingThreshold != defaultFiringThreshold {
-		t.Errorf("Default firingThreshold was %v, expected %v.", n.firingThreshold, defaultFiringThreshold)
-	}
+    if n.firingThreshold != defaultFiringThreshold {
+        t.Errorf("Default firingThreshold was %v, expected %v.", n.firingThreshold, defaultFiringThreshold)
+    }
 
-	if n.firingStrength != defaultFiringStrength {
-		t.Errorf("Default firingStrength was %v, expected %v.", n.firingStrength, defaultFiringStrength)
-	}
+    if n.firingStrength != defaultFiringStrength {
+        t.Errorf("Default firingStrength was %v, expected %v.", n.firingStrength, defaultFiringStrength)
+    }
 
-	if n.chargeDecayRate != defaultChargeDecayRate {
-		t.Errorf("Default chargeDecayRate was %v, expected %v.", n.chargeDecayRate, defaultChargeDecayRate)
-	}
-
-	if n.currentCharge != 0 {
-		t.Errorf("Default currentCharge was %v, expected 0.", n.currentCharge)
-	}
+    if n.currentCharge != 0 {
+        t.Errorf("Default currentCharge was %v, expected 0.", n.currentCharge)
+    }
 }
 
 func TestCharge(t *testing.T) {
-	n := NewNode()
-	n.Charge(0.5)
+    n := NewNode()
+    n.Charge(0.5)
 
-	if n.currentCharge-0.5 > delta {
-		t.Errorf("After charging by 0.5, charge was %v instead of 0.5!", n.currentCharge)
-	}
+    if n.currentCharge-0.5 > delta {
+        t.Errorf("After charging by 0.5, charge was %v instead of 0.5!", n.currentCharge)
+    }
 
-	n.Charge(0.7)
-	if n.currentCharge-0.2 > delta {
-		t.Errorf("Charge should have wrapped to 0.2. Got %v instead.", n.currentCharge)
-	}
+    n.Charge(0.7)
+    if n.currentCharge-1.2 > delta {
+        t.Errorf("Charge should now be 1.2. Got %v instead.", n.currentCharge)
+    }
 }
 
 func TestUpdate(t *testing.T) {
-	n := NewNode()
-	n.Update()
-	if n.currentCharge > 0 {
-		t.Errorf("Charge should still be 0 after update. Got %v instead.", n.currentCharge)
-	}
+    n := NewNode()
+    n.Work()
+    if n.currentCharge > 0 {
+        t.Errorf("Charge should still be 0 after update. Got %v instead.", n.currentCharge)
+    }
 
-	n.Charge(0.5)
-	n.Update()
-	if n.currentCharge-0.48 > delta {
-		t.Errorf("Charge should be 0.48 after update. Got %v instead.", n.currentCharge)
-	}
+    n.Charge(0.5)
+    n.Work()
+    if n.currentCharge-0.48 > delta {
+        t.Errorf("Charge should be 0.48 after update. Got %v instead.", n.currentCharge)
+    }
 }
 
 func TestFire(t *testing.T) {
-	n := NewNode()
-	m := NewNode()
-	n.AddOutput(m)
+    n := NewNode()
+    m := NewNode()
+    n.AddOutput(m)
 
-	n.Fire()
-	if m.currentCharge-0.8 > delta {
-		t.Errorf("m should have 0.8 charge after n fires. Got %v instead.", m.currentCharge)
-	}
+    n.Fire()
+    if m.currentCharge-0.8 > delta {
+        t.Errorf("m should have 0.8 charge after n fires. Got %v instead.", m.currentCharge)
+    }
 }
 
 func TestOutput(t *testing.T) {
-	n := NewNode()
-	m := NewNode()
-	n.AddOutput(m)
+    n := NewNode()
+    m := NewNode()
+    n.AddOutput(m)
 
-	n.Charge(1.2)
-	if m.currentCharge-0.8 > delta {
-		t.Errorf("m should have 0.8 charge after n fires. Got %v instead.", m.currentCharge)
-	}
+    n.Charge(1.2)
+    n.Work()
+    if m.currentCharge-0.8 > delta {
+        t.Errorf("m should have 0.8 charge after n fires. Got %v instead.", m.currentCharge)
+    }
 }
