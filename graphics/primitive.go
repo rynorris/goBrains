522f9eab13b4654b28b2de1e2ccd9e0c9db0784b
= <=>
2014-03-19 23:22:48 +0000

    Implemented the integer midpoint circle algorithm to make circle drawing better & faster.

--- graphics/primitive.go
+++ graphics/primitive.go
@@ -5,29 +5,33 @@
 
 package graphics
 
-import "math"
 import "image/color"
 import "github.com/banthar/Go-SDL/sdl"
 
+// A Primitive is a basic shape which can be drawn directly by the artist.
 type Primitive interface {
 	draw(s *sdl.Surface)
 }
 
+// A Point is as it sounds, a single point in space.
 type Point struct {
 	x, y int
 	c    color.Color
 }
 
+// Points are drawn by setting a single corresponding pixel.
 func (p Point) draw(s *sdl.Surface) {
-	s.Set(p.x, p.y, p.c)
+	safeSet(s, p.x, p.y, p.c)
 }
 
+// A Rectangle is... a rectangle.
 type Rectangle struct {
 	x, y int16
 	w, h uint16
 	c    color.Color
 }
 
+// Rectangles are drawn by directly calling FillRect on the surface.
 func (r Rectangle) draw(s *sdl.Surface) {
 	format := sdl.GetVideoInfo().Vfmt
 	color := sdl.ColorFromGoColor(r.c)
@@ -35,35 +39,86 @@ func (r Rectangle) draw(s *sdl.Surface) {
 	s.FillRect(&sdl.Rect{r.x, r.y, r.w, r.h}, colorVal)
 }
 
+// Circles are, you guessed it. Circles.
 type Circle struct {
 	x, y int16       // Location on screen
 	r    uint16      // Radius
-	b    int         // Border thickness
+	b    int         // Border thickness. For now only controls if there IS a border or not, not actually it's thickness.
 	c    color.Color // Color
 }
 
-// Ewwwwww
+// Circles may be filled or not.
 func (c Circle) draw(s *sdl.Surface) {
+	if c.b == 0 {
+		drawFilledCircle(c.x, c.y, c.r, c.c, s)
+	} else {
+		drawOutlineCircle(c.x, c.y, c.r, c.c, s)
+	}
+}
+
+// drawFilledCircle uses the integer midpoint circle algorithm to draw a filled
+// circle to the given surface.
+func drawFilledCircle(x0, y0 int16, r uint16, c color.Color, s *sdl.Surface) {
 	format := sdl.GetVideoInfo().Vfmt
-	color := sdl.ColorFromGoColor(c.c)
+	color := sdl.ColorFromGoColor(c)
 	colorVal := sdl.MapRGB(format, color.R, color.G, color.B)
 
-	prevL := int16(c.r)
-	for h := 0; h <= int(c.r); h++ {
-		l := int16(math.Sqrt(float64(int(c.r*c.r) - (h * h))))
+	x := int16(r)
+	y := int16(0)
+	e := 1 - x
 
-		x := int16(c.x) - l
+	for x >= y {
+		s.FillRect(&sdl.Rect{-x + x0, y + y0, uint16(2 * x), 1}, colorVal)
+		s.FillRect(&sdl.Rect{-x + x0, -y + y0, uint16(2 * x), 1}, colorVal)
+		s.FillRect(&sdl.Rect{-y + x0, x + y0, uint16(2 * y), 1}, colorVal)
+		s.FillRect(&sdl.Rect{-y + x0, -x + y0, uint16(2 * y), 1}, colorVal)
 
-		if c.b == 0 {
-			s.FillRect(&sdl.Rect{x, c.y + int16(h), uint16(2 * l), 1}, colorVal)
-			s.FillRect(&sdl.Rect{x, c.y - int16(h), uint16(2 * l), 1}, colorVal)
+		y++
+
+		if e < 0 {
+			e += 2*y + 1
 		} else {
-			s.FillRect(&sdl.Rect{x, c.y + int16(h), uint16(math.Max(1, math.Abs(float64(l-prevL)))), 1}, colorVal)
-			s.FillRect(&sdl.Rect{x, c.y - int16(h), uint16(math.Max(1, math.Abs(float64(l-prevL)))), 1}, colorVal)
-			s.FillRect(&sdl.Rect{x + l + prevL, c.y + int16(h), uint16(math.Max(1, math.Abs(float64(l-prevL)))), 1}, colorVal)
-			s.FillRect(&sdl.Rect{x + l + prevL, c.y - int16(h), uint16(math.Max(1, math.Abs(float64(l-prevL)))), 1}, colorVal)
+			x--
+			e += 2 * (y - x + 1)
 		}
+	}
+}
 
-		prevL = l
+// drawOutlineCircle uses the integer midpoint circle algorithm to draw the outline
+// of a circle (1 px thick) to the given surface.
+func drawOutlineCircle(x0, y0 int16, r uint16, c color.Color, s *sdl.Surface) {
+	s.Lock()
+	defer s.Unlock()
+
+	color := sdl.ColorFromGoColor(c)
+
+	x := int16(r)
+	y := int16(0)
+	e := 1 - x
+
+	for x >= y {
+		safeSet(s, int(x+x0), int(y+y0), color)
+		safeSet(s, int(x+x0), int(-y+y0), color)
+		safeSet(s, int(-x+x0), int(y+y0), color)
+		safeSet(s, int(-x+x0), int(-y+y0), color)
+		safeSet(s, int(y+x0), int(x+y0), color)
+		safeSet(s, int(y+x0), int(-x+y0), color)
+		safeSet(s, int(-y+x0), int(x+y0), color)
+		safeSet(s, int(-y+x0), int(-x+y0), color)
+
+		y++
+
+		if e < 0 {
+			e += 2*y + 1
+		} else {
+			x--
+			e += 2 * (y - x + 1)
+		}
+	}
+}
+
+func safeSet(s *sdl.Surface, x, y int, c sdl.Color) {
+	if x >= 0 && y >= 0 && x < int(s.W) && y < int(s.H) {
+		s.Set(x, y, c)
 	}
 }
