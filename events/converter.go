365853f6a4daf8c8df710a62e2e8351ba63c6dfd
= <=>
2014-03-22 19:50:39 +0000

    Moved keybindings into a map for even better clarity and extensibility.

--- events/converter.go
+++ events/converter.go
@@ -4,35 +4,34 @@ import "github.com/banthar/Go-SDL/sdl"
 
 var (
 	// Key Bindings
-	terminate   = sdl.K_ESCAPE
-	toggle_draw = sdl.KEY_z
+	keyDownBindings = map[uint32]EventType{
+		sdl.K_z:      TOGGLE_DRAW,
+		sdl.K_ESCAPE: TERMINATE,
+	}
 )
 
 // Listens for events from SDL, converts them into
 // game events, and pipes them down the given channel
 func Poll(out chan Event) {
 	for {
-		out <- convert(sdl.WaitEvent())
+		if e := convert(sdl.WaitEvent()); e.GetType() != NONE {
+			out <- e
+		}
 	}
 }
 
 // Converts SDL Events into game events.
 func convert(e sdl.Event) Event {
-	switch e.(type) {
+	switch e := e.(type) {
 	case sdl.KeyboardEvent:
 		// Keyboard event handling.
-		e := sdl.KeyboardEvent(e)
 		switch e.Type {
 		case sdl.KEYDOWN:
 			// Key Down handling
-			switch e.Keysym {
-			case terminate:
-				// Close the program
-				out <- BasicEvent{TERMINATE}
-			case toggle_draw:
-				// Turn on/off drawing graphics
-				out <- BasicEvent{TOGGLE_DRAW}
+			if t, ok := keyDownBindings[e.Keysym.Sym]; ok {
+				return BasicEvent{t}
 			}
 		}
 	}
+	return BasicEvent{NONE}
 }
