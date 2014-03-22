797ca30264803ad3bc545b70236c93aeb633ba91
= <=>
2014-03-22 15:49:23 +0000

    ...3

--- install_go_sdl.sh
+++ install_go_sdl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd $HOME/gopath/src
-mkdir -p github.com/banthar/Go-SDL
-cd github.com/banthar/Go-SDL
-git clone git@github.com:banthar/Go-SDL.git
+mkdir -p github.com/banthar
+cd github.com/banthar
+yes yes | git clone git@github.com:banthar/Go-SDL.git
 make
