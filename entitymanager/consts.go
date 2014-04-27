2ca638889521db4d2dacaefe0be31d0a82248b0c
= <=>
2014-04-27 22:56:57 +0100

    Added some constants for entitymanager

---
+++ entitymanager/consts.go
@@ -0,0 +1,10 @@
+package entitymanager
+
+const (
+	max_creatures       = 50   // Maximum creatures in play at once. Stop breeding if this many creatures live.
+	max_food            = 50   // Maximum number of food blobs in play at once.
+	breeding_rate       = 1000 // Spawn a new creature every this many ticks.
+	food_replenish_rate = 1000 // Spawn a new blob of food every this many ticks.
+	initial_creatures   = 25   // Start with this many random creatures.
+	inital_food         = 25   // Start with this many randomly placed food blobs.
+)
