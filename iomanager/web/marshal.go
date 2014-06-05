f51f1e86139d81c27ec441f021b2f48c31398372
Ryan Norris <rynorris@gmail.com>
2014-06-05 18:45:19 +0100

    Added support for marshaling entities to send down the wire.

---
+++ iomanager/web/marshal.go
@@ -0,0 +1,61 @@
+package web
+
+import (
+	"encoding/json"
+
+	"github.com/DiscoViking/goBrains/creature"
+	"github.com/DiscoViking/goBrains/food"
+	"github.com/DiscoViking/goBrains/iomanager"
+)
+
+// Turns a list of DrawSpecs into a json string to be sent over the wire.
+// We have to do some work ourselves since we need to represent different
+// entity types differently. However we use the json package to do some of the
+// work.
+func marshal(data []iomanager.DrawSpec) string {
+	output := `
+{"scene": {
+  "entities": [`
+
+	for _, spec := range data {
+		output += marshalOne(spec)
+		output += ","
+	}
+
+	output += "]}}"
+
+	return output
+}
+
+// Marshal a single DrawSpec into json.
+func marshalOne(spec iomanager.DrawSpec) string {
+	var output []byte
+	var err error
+
+	switch e := spec.E.(type) {
+	case *creature.Creature:
+		s := creatureSpec{
+			entitySpec{"creature", int(spec.Loc.X), int(spec.Loc.Y)},
+			e.Color(),
+			spec.Loc.Orient,
+		}
+		output, err = json.Marshal(s)
+
+	case *food.Food:
+		s := foodSpec{
+			entitySpec{"food", int(spec.Loc.X), int(spec.Loc.Y)},
+			int(e.Radius()),
+		}
+		output, err = json.Marshal(s)
+
+	default:
+		s := entitySpec{"unknown", int(spec.Loc.X), int(spec.Loc.Y)}
+		output, err = json.Marshal(s)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(output)
+}
---
+++ iomanager/web/marshal_test.go
@@ -0,0 +1,31 @@
+package web
+
+import (
+	"testing"
+
+	"github.com/DiscoViking/goBrains/creature"
+	"github.com/DiscoViking/goBrains/food"
+	io "github.com/DiscoViking/goBrains/iomanager"
+	lm "github.com/DiscoViking/goBrains/locationmanager"
+)
+
+func TestMarshal(t *testing.T) {
+	l := lm.NewLocationManager(200, 200)
+	testcases := map[io.DrawSpec]string{
+		io.DrawSpec{
+			creature.NewSimple(l), lm.Combination{10, 20, 2.5},
+		}: `{"Type":"creature","X":10,"Y":20,"Colour":{"R":200,"G":50,"B":50,"A":255},"Angle":2.5}`,
+
+		io.DrawSpec{
+			food.New(l, 100), lm.Combination{10, 20, 2.5},
+		}: `{"Type":"food","X":10,"Y":20,"Size":10}`,
+	}
+
+	for in, out := range testcases {
+		marshaled := marshalOne(in)
+
+		if marshaled != out {
+			t.Errorf("Expected: %v\nGot: %V\n", out, marshaled)
+		}
+	}
+}
